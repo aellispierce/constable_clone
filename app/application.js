5de120f0dc310e054861b9b9f21639e58f7a15fb
Ashley Ellis <anellis12@gmail.com>
2016-03-21 15:12:34 -0400

    Use ES6 classes

---
+++ app/application.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var root = 'http://localhost:4000/api';
+var origin = window.location.origin;
+
+var App = {
+  init: function init() {
+    $("body").prepend(
+        `<a href = ${origin}/new.html> + </a>`)
+  },
+  constableApi: class ConstableApi {
+
+    getAnnouncements(){
+      $.ajax({
+        url: root + '/announcements',
+        method: 'GET'
+      }).done(function(data) {
+        var announcements = data.announcements;
+
+        announcements.forEach(function(announcement){
+          $("#response").append(
+              `<a href= ${origin}/announcement.html?id=${(announcement["id"])}>
+              ${announcement["title"]} <br/> <br/></a>`)
+        });
+      });
+    }
+
+    showAnnouncement(id){
+      $.ajax({
+        url: root + '/announcements/' + id,
+        method: 'GET'
+      }).done(function(data) {
+        $("#response").append(
+            `${data.announcement["body"]} <br/> <br/>`)
+      });
+    }
+
+    newAnnouncement(){
+      $.ajax({
+        url: root + '/announcements',
+        method: 'post',
+        dataType: 'application/json',
+        data: $("#new").serializeArray()
+      }).done(function(data) {
+        $("#response").append(
+          `${data.announcement["body"]} <br/> <br/>`)
+      });
+    }
+  }
+};
+
+module.exports = App
