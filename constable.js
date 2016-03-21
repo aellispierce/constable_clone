4411ec12531d693a02ebe30101aff8eaf6cb0af5
Ashley Ellis <anellis12@gmail.com>
2016-03-21 16:03:33 -0400

    Move scripts into constable.js

---
+++ constable.js
@@ -0,0 +1,39 @@
+  var root = 'http://localhost:4000/api';
+  var local = 'file:///Users/ashley/thoughtbot/constable_clone/';
+
+  function getAnnouncements(){
+    $.ajax({
+      url: root + '/announcements',
+      method: 'GET'
+    }).done(function(data) {
+      console.log(data);
+      var announcements = data.announcements;
+
+      announcements.forEach(function(announcement){
+      $("#response").append(
+          '<a href=' + local + '/announcement.html?id=' + (announcement['id']) + '>'+
+          announcement['title'] + " " +"<br/> <br/>"+
+          '</a>')
+      });
+    });
+  };
+
+  function showAnnouncement(id){
+    $.ajax({
+      url: root + '/announcements/' + id,
+      method: 'GET'
+    }).done(function(data) {
+      console.log(data);
+
+      $("#response").append(
+        data.announcement['body'] + " " +"<br/> <br/>")
+    });
+  };
+
+  function search(query){
+    $.ajax({
+      url: root + '/searches',
+      method: 'POST',
+      data: {query: query }
+    });
+  };
