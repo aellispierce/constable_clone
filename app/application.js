71a2111f366a80d7f18de1db0f86aae5c5278da3
Ashley Ellis <anellis12@gmail.com>
2016-03-28 09:55:25 -0400

    Add brunch with babel for es6

---
+++ app/application.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var root = 'http://localhost:4000/api';
+var origin = window.location.origin;
+
+var App = {
+    init: function init() {
+          console.log('App initialized.');
+            },
+    getAnnouncements: function getAnnouncements(){
+      $.ajax({
+        url: root + '/announcements',
+        method: 'GET'
+      }).done(function(data) {
+        var announcements = data.announcements;
+
+        announcements.forEach(function(announcement){
+        $("#response").append(
+          `<a href= ${origin}/announcement.html?id=${(announcement["id"])}>
+          ${announcement["title"]} <br/> <br/></a>`)
+        });
+      });
+    },
+
+    showAnnouncement: function showAnnouncement(id){
+      $.ajax({
+        url: root + '/announcements/' + id,
+        method: 'GET'
+      }).done(function(data) {
+        $("#response").append(
+          `${data.announcement["body"]} <br/> <br/>`)
+      });
+    },
+};
+
+module.exports = App
