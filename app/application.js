f29e68ba550ccc80efd858eb5f05e94da7e1f0da
Ashley Ellis <anellis12@gmail.com>
2016-03-29 09:29:10 -0400

    Class it up

--- app/application.js
+++ app/application.js
@@ -4,11 +4,13 @@ var root = 'http://localhost:4000/api';
 var origin = window.location.origin;
 
 var App = {
-    init: function init() {
-          $("body").prepend(
-              `<a href = ${origin}/new.html> + </a>`)
-            },
-    getAnnouncements: function getAnnouncements(){
+  init: function init() {
+    $("body").prepend(
+        `<a href = ${origin}/new.html> + </a>`)
+  },
+  constableApi: class ConstableApi {
+
+    getAnnouncements(){
       $.ajax({
         url: root + '/announcements',
         method: 'GET'
@@ -16,21 +18,36 @@ var App = {
         var announcements = data.announcements;
 
         announcements.forEach(function(announcement){
-        $("#response").append(
-          `<a href= ${origin}/announcement.html?id=${(announcement["id"])}>
-          ${announcement["title"]} <br/> <br/></a>`)
+          $("#response").append(
+              `<a href= ${origin}/announcement.html?id=${(announcement["id"])}>
+              ${announcement["title"]} <br/> <br/></a>`)
         });
       });
-    },
-    showAnnouncement: function showAnnouncement(id){
+    }
+
+    showAnnouncement(id){
       $.ajax({
         url: root + '/announcements/' + id,
         method: 'GET'
       }).done(function(data) {
         $("#response").append(
+            `${data.announcement["body"]} <br/> <br/>`)
+      });
+    }
+
+    newAnnouncement(){
+      console.log("MADE IT");
+      $.ajax({
+        url: root + '/announcements',
+        method: 'post',
+        dataType: 'application/json',
+        data: $("#new").serializeArray()
+      }).done(function(data) {
+        $("#response").append(
           `${data.announcement["body"]} <br/> <br/>`)
       });
-    },
+    }
+  }
 };
 
 module.exports = App
