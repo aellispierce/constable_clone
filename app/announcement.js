6c390feaf660792ea8b734126413fc2513a5b9f3
Ashley Ellis <anellis12@gmail.com>
2016-03-30 15:44:34 -0400

    More refactoring

--- app/announcement.js
+++ app/announcement.js
@@ -17,16 +17,31 @@ class Announcement {
     });
   }
 
-  show(data){
-    $("#response").append(
-        `${data.announcement["body"]} <br/> <br/>`)
+  show(id){
+    this.find(id).done(function(data){
+      $("#response").append(
+          `${data.announcement["body"]} <br/> <br/>`)
+    });
   }
 
-  index(data){
-    var announcements = data.announcements;
-    announcements.forEach(function(announcement){
-      $("#response").append(path.link(announcement["id"], announcement["title"]))
+  index(){
+    this.all().done(function(data){
+      data.announcements.forEach(function(announcement){
+        $("#response").append(path.link(announcement["id"], announcement["title"]))
+      });
     });
+  }
+
+  validate(e){
+    e.preventDefault();
+    const formData= $("#new").serializeArray();
+    if(formData[0]["value"] == ""){
+      $("body").append('Title is required');
+    } else if (formData[1]["value"] == ""){
+      $("body").append('Body is required');
+    } else {
+      announcement.create()
+    }
   }
 }
 
